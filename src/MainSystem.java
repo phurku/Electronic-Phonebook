777a3d8c7fb74ef881c85d0429e1d2ba0ea9431c
phurku <phurku2017@gmail.com>
2024-05-08 20:56:11 +1000

    Read and adding new record completed

--- src/MainSystem.java
+++ src/MainSystem.java
@@ -6,44 +6,102 @@ import java.util.Scanner;
 public class MainSystem {
     public static void main(String[] args) {
         try {
-            // Read from phonebook.txt
             File phonebookFile = new File("src/phonebook.txt");
-            Scanner phonebookScanner = new Scanner(phonebookFile);
-            while (phonebookScanner.hasNextLine()) {
-                String line = phonebookScanner.nextLine();
-                System.out.println(line);
-            }
-            phonebookScanner.close();
-
-            // Read from instructions.txt and write to newRecords.txt
             File instructionsFile = new File("src/instructions.txt");
             File outputFile = new File("src/newRecords.txt");
-            Scanner instructionsScanner = new Scanner(instructionsFile);
-            PrintWriter outputWriter = new PrintWriter(outputFile);
 
-            while (instructionsScanner.hasNextLine()) {
-                String instruction = instructionsScanner.nextLine();
-                if (instruction.startsWith("add")) {
-                    String[] recordDetails = instruction.split(" ");
-                    Record record = new Record();
-                    record.setName(recordDetails[1]);
-                    record.setPhone(recordDetails[2]);
-                    record.setEmail(recordDetails[3]);
-                    record.setAddress(recordDetails[4]);
-                    record.setBirthday(recordDetails[5]);
+            Scanner inputScanner = new Scanner(System.in);
+            boolean exit = false;
 
-                    outputWriter.println(record.getName());
-                    outputWriter.println(record.getPhone());
-                    outputWriter.println(record.getEmail());
-                    outputWriter.println(record.getAddress());
-                    outputWriter.println(record.getBirthday());
+            while (!exit) {
+                System.out.println("Choose an option:");
+                System.out.println("1. Add record");
+                System.out.println("2. Delete record");
+                System.out.println("3. Query records");
+                System.out.println("4. Exit");
+
+                int choice = inputScanner.nextInt();
+                inputScanner.nextLine(); // Consume the newline character
+
+                switch (choice) {
+                    case 1:
+                        addRecord(instructionsFile, outputFile, inputScanner);
+                        break;
+                    case 2:
+                        deleteRecord(phonebookFile, outputFile, inputScanner);
+                        break;
+                    case 3:
+                        queryRecords(phonebookFile);
+                        break;
+                    case 4:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
                 }
             }
 
-            instructionsScanner.close();
-            outputWriter.close();
+            inputScanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         }
+    }
+
+    private static void addRecord(File instructionsFile, File outputFile, Scanner inputScanner) throws FileNotFoundException {
+        System.out.println("Enter record details (name, phone, email, address, birthday):");
+        String name = inputScanner.nextLine();
+        String phone = inputScanner.nextLine();
+        String email = inputScanner.nextLine();
+        String address = inputScanner.nextLine();
+        String birthday = inputScanner.nextLine();
+
+        PrintWriter outputWriter = new PrintWriter(outputFile);
+        outputWriter.println("Added record " + name + " " + phone + " " + email + " " + address + " " + birthday);
+        outputWriter.close();
+    }
+
+    private static void deleteRecord(File phonebookFile, File outputFile, Scanner inputScanner) throws FileNotFoundException {
+        System.out.println("Enter the name of the record to delete:");
+        String nameToDelete = inputScanner.nextLine();
+
+        Scanner phonebookScanner = new Scanner(phonebookFile);
+        PrintWriter outputWriter = new PrintWriter(outputFile);
+
+        boolean recordFound = false;
+
+        while (phonebookScanner.hasNextLine()) {
+            String name = phonebookScanner.nextLine();
+            String phone = phonebookScanner.nextLine();
+            String email = phonebookScanner.nextLine();
+            String address = phonebookScanner.nextLine();
+            String birthday = phonebookScanner.nextLine();
+
+            if (!name.equals(nameToDelete)) {
+                outputWriter.println(name);
+                outputWriter.println(phone);
+                outputWriter.println(email);
+                outputWriter.println(address);
+                outputWriter.println(birthday);
+            } else {
+                recordFound = true;
+                System.out.println("Record deleted: " + name);
+            }
+        }
+
+        if (!recordFound) {
+            System.out.println("Record not found.");
+        }
+
+        phonebookScanner.close();
+        outputWriter.close();
+    }
+
+    private static void queryRecords(File phonebookFile) throws FileNotFoundException {
+        Scanner phonebookScanner = new Scanner(phonebookFile);
+        while (phonebookScanner.hasNextLine()) {
+            String line = phonebookScanner.nextLine();
+            System.out.println(line);
+        }
+        phonebookScanner.close();
     }
 }
